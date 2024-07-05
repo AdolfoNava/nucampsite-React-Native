77133726003c9839c1ca96a6249de5ae0958e403
AdolfoNava <adolfonava9960@gmail.com>
2024-07-05 15:19:58 -0500

    Favorites implementation

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -128,7 +128,28 @@ const ReservationNavigator = () => {
     </Stack.Navigator>
   )
 }
-
+const FavoritesNavigator = () => {
+  const Stack = createStackNavigator();
+  return (
+    <Stack.Navigator screenOptions={screenOptions}>
+      <Stack.Screen
+        name='Favorites'
+        component={FavoritesScreen}
+        options={({ navigation }) => ({
+          title: 'Favorite Campsites',
+          headerLeft: () => (
+            <Icon
+              name='heart'
+              type='font-awesome'
+              iconStyle={styles.stackIcon}
+              onPress={() => navigation.toggleDrawer()}
+            />
+          )
+        })}
+      />
+    </Stack.Navigator>
+  );
+};
 const CustomDrawerContent = (props) => {
   return (
     <DrawerContentScrollView {...props}>
@@ -224,6 +245,22 @@ const Main = () => {
             drawerIcon: ({ color }) => (
               <Icon
                 name='address-card'
+                type='font-awesome'
+                size={24}
+                iconStyle={{ width: 24 }}
+                color={color}
+              />
+            )
+          }}
+        />
+        <Drawer.Screen
+          name='Favorites'
+          component={FavoritesNavigator}
+          options={{
+            title: 'My Favorites',
+            drawerIcon: ({ color }) => (
+              <Icon
+                name='heart'
                 type='font-awesome'
                 size={24}
                 iconStyle={{ width: 24 }}
